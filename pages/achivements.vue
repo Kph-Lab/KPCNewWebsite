abb9f476e852f8dba336242d9f7af76f1a9eb52e
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-01-07 13:32:29 +0900

    indexworksのレスポンシブデザイン対応

--- pages/achivements.vue
+++ pages/achivements.vue
@@ -3,14 +3,30 @@
     <h1 class="text-6xl font-bold text-center mt-32 drop-shadow-lg">Achivements</h1>
     <p class="text-center">ぶつりぶのさくひんしょうかいぺーじだよ</p>
     <div class="grid grid-cols-1 gap-4 mt-4">
-      <div class="p-4 bg-white/40 border-2 border-white/20 rounded-2xl flex flex-row items-center gap-2">
+      <div
+        v-for="achivement in achivements"
+        :key="achivement.key"
+        class="p-4 bg-white/40 border-2 border-white/20 rounded-2xl flex flex-row items-center gap-2"
+      >
         <i class="fa-solid fa-trophy text-4xl"></i>
         <div>
-          <h1 class="font-bold text-2xl">なんかの大会</h1>
-          <p class="text-black/60 text-[12px]">受賞者A・受賞者B</p>
+          <h1 class="font-bold text-2xl">{{achivement.name}}</h1>
+          <p class="text-black/60 text-[12px]">{{achivement.who}}</p>
         </div>
       </div>
     </div>
   </div>
   <Footer/>
 </template>
+<script>
+export default {
+  data(){return{
+    achivements: [
+      {
+        name: "なんかの大会",
+        who: "受賞者A・受賞者B"
+      }
+    ]
+  }}
+}
+</script>
--- pages/works.vue
+++ pages/works.vue
@@ -3,15 +3,41 @@
     <h1 class="text-6xl font-bold text-center mt-32 drop-shadow-lg">Works</h1>
     <p class="text-center">ぶつりぶのさくひんしょうかいぺーじだよ</p>
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-      <div class="p-2 bg-white/40 border-2 border-white/20 rounded-2xl flex flex-col gap-2">
-        <div class="basis-40 bg-slate-500 rounded-md"></div>
+      <div
+        class="p-2 bg-white/40 border-2 border-white/20 rounded-2xl flex flex-col gap-2"
+         v-for="work in works"
+        :key="work.key"
+      >
+        <div class="relative bg-center bg-cover before:pt-[60%] before:block rounded-md" :style="`background-image:url(${work.image.url})`"/>
         <div>
-          <h1 class="font-bold text-2xl">LED CUBE</h1>
-          <p class="text-sm">LED CUBEすげええええええええええええ</p>
-          <p class="text-black/40 text-[12px]">作成者:せきねせんぱい</p>
+          <h1 class="font-bold text-2xl">{{work.name}}</h1>
+          <p class="text-sm">{{work.description}}</p>
+          <p class="text-black/40 text-[12px]">作成者:{{work.creator}}</p>
         </div>
       </div>
     </div>
   </div>
   <Footer/>
 </template>
+<script>
+import { createClient } from "microcms-js-sdk"
+export default {
+  data(){return{
+    works: []
+  }},
+  mounted(){
+    const config = useRuntimeConfig()
+    const client = createClient({
+      serviceDomain: config.public.serviceDomain,
+      apiKey: config.public.apiKey
+    })
+    client
+    .get({
+      endpoint: 'works'
+    })
+    .then((res) => {
+      this.works = res.contents
+    });
+  }
+}
+</script>
