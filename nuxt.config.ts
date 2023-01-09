118f21e17ba0b85c3bf73fd7b7271bb3cc45425b
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-01-09 21:47:44 +0900

    set SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,18 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "海城中高物理部",
+      meta: [
+        {charset: "utf-8"},
+        {name: "viewport", content: "width=device-width,initial-scale=1"},
+        {name: "description", content: "海城中高物理部の公式ホームページです。記事や作品などの情報を発信しています。"},
+        {property: "og:title", content: "海城中高物理部"},
+        {property: "og:description", content: "海城中高物理部の公式ホームページです。記事や作品などの情報を発信しています。"},
+        {property: "og:image", content: "https://images.microcms-assets.io/assets/5ff6d04dc95341adae4d92105745e972/3da61f9535f841d393cd443047137ed5/OGP_image.png"},
+        {property: "twitter:card", content: "summary_large_image"},
+        {property: "twitter:site", content: "@kaijo_physics"},
+        {property: "og:locale", content: "ja_JP"}
+      ],
       script: [
         {src:"https://kit.fontawesome.com/9f3463b573.js", crossorigin:"anonymous"}
       ]
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="fixed h-screen w-screen grid place-content-center">
-    <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-cyan-300 to-purple-300"/>
-    <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-purple-300 to-rose-200 -mt-24"/>
-    <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-blue-300 to-teal-200 -mt-24"/>
-    <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-red-300 -mt-24"/>
-  </div>
-  <div class="relative snap-y snap-mandatory overflow-y-auto h-screen w-screen" id="main" @scroll="moveBgAnime">
-    <IndexTop class="snap-center h-screen"/>
-    <IndexAbout class="snap-center h-screen"/>
-    <IndexWorks class="snap-center h-screen"/>
-    <IndexArticles class="snap-start h-screen"/>
-    <IndexAchivements class="snap-center h-screen"/>
-    <Footer class="snap-end"/>
-  </div>
-  <div class="fixed bottom-14 -right-6 rotate-90">
-    <p class="text-sm tracking-[0.25rem] -mb-1 ml-5 text-black/30">scroll</p>
-    <div
-      class="h-[8px] w-[100px] border-black/30 border-b-[1px] border-r-[1px]"
-      style="transform:skew(45deg)"
-    />
+  <div>
+    <div class="fixed h-screen w-screen grid place-content-center">
+      <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-cyan-300 to-purple-300"/>
+      <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-purple-300 to-rose-200 -mt-24"/>
+      <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-gradient-to-tr from-blue-300 to-teal-200 -mt-24"/>
+      <div class="bg-circle opacity-0 w-24 h-24 rounded-full bg-red-300 -mt-24"/>
+    </div>
+    <div class="relative snap-y snap-mandatory overflow-y-auto h-screen w-screen" id="main" @scroll="moveBgAnime">
+      <IndexTop class="snap-center h-screen"/>
+      <IndexAbout class="snap-center h-screen"/>
+      <IndexWorks class="snap-center h-screen"/>
+      <IndexArticles class="snap-start h-screen"/>
+      <IndexAchivements class="snap-center h-screen"/>
+      <Footer class="snap-end"/>
+    </div>
+    <div class="fixed bottom-14 -right-6 rotate-90">
+      <p class="text-sm tracking-[0.25rem] -mb-1 ml-5 text-black/30">scroll</p>
+      <div
+        class="h-[8px] w-[100px] border-black/30 border-b-[1px] border-r-[1px]"
+        style="transform:skew(45deg)"
+      />
+    </div>
   </div>
 </template>
 <style>
